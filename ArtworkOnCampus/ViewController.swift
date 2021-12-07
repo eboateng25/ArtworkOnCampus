c02930df3774796dee4208bcb2de2b4026bb01bb
Emanuel Boateng <sgeboate@liverpool.ac.uk>
2021-12-07 15:25:30 +0000

    Initial Commit

---
+++ ArtworkOnCampus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtworkOnCampus
+//
+//  Created by Boateng, Emanuel on 07/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
